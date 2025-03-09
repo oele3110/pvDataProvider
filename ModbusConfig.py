12e9512b60884309f40be89971c2d6d77448b113
Dennis Lampert <oele3110@hotmail.com>
2025-03-09 21:57:12 +0100

    First websocket example

---
+++ ModbusConfig.py
@@ -0,0 +1,14 @@
+from modbusReader.ModbusDataType import ModbusDataType
+
+modbus_wallbox_config = {
+    "home_consumption":
+        {
+            "address": 40982,
+            "type": ModbusDataType.UINT32,
+            "resolution": 1,
+            "digits_round": 0,
+            "update_frequency": 1,
+            "display_string": "Home Consumption",
+            "unit": "W"
+        }
+}
